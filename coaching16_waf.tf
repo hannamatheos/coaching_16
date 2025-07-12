559335b88afb4f525b5ce5986627d701c65a3cce
darknightKJ <krisbianto@gmail.com>
2025-07-12 11:18:41 +0800

    Add AWS WAF configuration for coaching16 v2

--- coaching16_waf.tf
+++ coaching16_waf.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_wafv2_web_acl" "coaching16" {
   name        = "coaching16_waf"
   scope       = "REGIONAL"

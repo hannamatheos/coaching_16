97bee4ac10cc5b7900672344b0ceddf497525410
darknightKJ <krisbianto@gmail.com>
2025-07-12 11:12:50 +0800

    Add AWS WAF configuration for coaching16

---
+++ coaching16_waf.tf
@@ -0,0 +1,37 @@
+resource "aws_wafv2_web_acl" "coaching16" {
+  name        = "coaching16_waf"
+  scope       = "REGIONAL"
+  description = "WAF for coaching16"
+
+  default_action {
+    allow {}
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "coaching16WAF"
+    sampled_requests_enabled   = true
+  }
+
+  rule {
+    name     = "limit-ip"
+    priority = 1
+
+    action {
+      block {}
+    }
+
+    statement {
+      rate_based_statement {
+        limit              = 100
+        aggregate_key_type = "IP"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "limitIP"
+      sampled_requests_enabled   = true
+    }
+  }
+}
